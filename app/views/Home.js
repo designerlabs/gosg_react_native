abacd266eac3d763b73788d713cd10cdfab22f29
sridhar <sridhar@MiMobile-Macbook.local>
2018-06-20 20:51:28 +0800

    updated

---
+++ app/views/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '../sections/Header.js';
+import { Hero } from '../sections/Hero.js';
+import { Menu } from '../sections/Menu.js';
+
+export class Home extends React.Component{
+    render(){
+        return (
+            <View style={styles.container}>
+                <Header message = 'Press to Login' />
+                <Hero />
+                <Menu />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    }
+})
